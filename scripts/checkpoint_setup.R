8ba2dbe797560d6a31f49766b57d4f44c89ba97b
Matt Leary <33221468+mleary@users.noreply.github.com>
2021-11-19 23:52:28 -0500

    add checkpoint, workflow

---
+++ scripts/checkpoint_setup.R
@@ -0,0 +1,7 @@
+library(checkpoint)
+
+checkpoint::checkpoint(
+  "2021-11-01",
+  R.version = '4.0.5',
+  checkpoint_location = here::here()
+)
---
+++ scripts/run_rmd_file.R
@@ -0,0 +1,5 @@
+.libPaths("./.checkpoint/2021-11-01/lib/x86_64-pc-linux-gnu/4.0.5/")
+
+rmarkdown::render(
+  input = "./index.Rmd",
+)
